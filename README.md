668b0fd4528b661e14448ed78301fa1edcd3920c
Nguyen Trieu <nguyentrieussj4@gmail.com>
2021-06-07 17:06:07 +0700

    first commit, try chatengine.io

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+# You can test with user01 and user02 with password is: 123456
+
 # Getting Started with Create React App
 
 This project was bootstrapped with [Create React App](https://github.com/facebook/create-react-app).
--- src/App.js
+++ src/App.js
@@ -1,21 +1,19 @@
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
+import LoginForm from "./components/LoginForm";
+import ChatFeed from "./components/ChatFeed";
+
 function App() {
+  if (!localStorage.getItem("username")) return <LoginForm />;
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height="100vh"
+      projectID="bc47ab13-e367-4d49-bbe4-0ca0ef86ca37"
+      userName={localStorage.getItem("username")}
+      userSecret={localStorage.getItem("password")}
+      renderChatFeed={(chatAppProps) => <ChatFeed {...chatAppProps} />}
+    />
   );
 }
 
